cff109528c5c98d97ed387123cb8697a4215d6a8
ramSeraph <kandimalla.sreeram@gmail.com>
2022-09-12 23:49:56 +0530

    more attempts

--- .github/scripts/rfuncs.sh
+++ .github/scripts/rfuncs.sh
@@ -10,18 +10,18 @@ function record_call {
 function get_release_id {
     record_call "$@"
     set +e
-    release_id="$(gh api -H $gh_headers /repos/${GITHUB_REPOSITORY}/releases/tags/$1 2>>$err_file | jq '.id' 2>>$err_file)"
+    release_id="$(gh api -H "$gh_headers" /repos/${GITHUB_REPOSITORY}/releases/tags/$1 2>>$err_file | jq '.id' 2>>$err_file)"
     set -e
     echo $release_id
 }
 
 function has_release {
     record_call "$@"
-    rname=$1
-    release_names="$(gh api -H $gh_headers /repos/${GITHUB_REPOSITORY}/releases 2>>$err_file | jq -r '.[].name' 2>>$err_file)"
+    export rname=$1
+    release_names="$(gh api -H "$gh_headers" /repos/${GITHUB_REPOSITORY}/releases 2>>$err_file | jq -r '.[].name' 2>>$err_file)"
     set +e
     echo "$release_names" | grep -q "^$rname$" 2>>$err_file
-    if [[ $? ]]; then
+    if [[ $? == 0 ]]; then
         echo "yes"
     else
         echo "no"

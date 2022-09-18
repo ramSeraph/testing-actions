3af26f17cabdb756c832ea75eebec5adfcba9b35
ramSeraph <kandimalla.sreeram@gmail.com>
2022-09-18 17:14:18 +0530

    klsfl

--- .github/scripts/cfuncs.sh
+++ .github/scripts/cfuncs.sh
@@ -5,6 +5,6 @@ if [[ -z "$GITHUB_REPOSITORY" ]]; then
     exit 1
 fi
 
-headers="-H 'Accept: application/vnd.github+json'" 
-cache_info=$(gh api "$headers" "/repos/${GITHUB_REPOSITORY}/actions/caches")
+gh_headers="Accept: application/vnd.github+json" 
+cache_info=$(gh api -H "$gh_headers" "/repos/${GITHUB_REPOSITORY}/actions/caches")
 echo "$cache_info"

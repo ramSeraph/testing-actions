569fdbe2f9ea45c5e101dd350aa7ff8cfc941274
ramSeraph <kandimalla.sreeram@gmail.com>
2022-10-05 02:03:37 +0530

    just work

--- .github/scripts/cfuncs.sh
+++ .github/scripts/cfuncs.sh
@@ -46,11 +46,12 @@ function get_old_ids {
         echo "older_than_in_days param $older_than_in_days is not a positive number" >>$err_file
         exit 1
     fi
-    cutoff=$(( curr_time - (86400 * older_than_in_days) ))
+    echo "curr_time: $curr_time" >>$errfile
+    echo "older than: $older_than_in_days" >>$errfile
+    cutoff=$(echo $curr_time - 86400*$older_than_in_days | bc -l)
     cutoff=$( echo ${cutoff}/1 | bc )
     echo "cuttoff: $cutoff" >>$errfile 
 
-    #ids="$(get_cache_info | jq --arg c "$cutoff" --arg f "$match" '.actions_caches[] | select(.last_accessed_at | sub("\\.[0-9]+Z$";"Z") | fromdateiso8601 < ($c | tonumber)) | select(.key | test($f)) | .id' 2>>$err_file)"
     ids="$(get_cache_info | jq --arg c "$cutoff" --arg f "$match" '.actions_caches[] | select(.last_accessed_at | sub("\\.[0-9]+Z$";"Z") | fromdateiso8601 < ($c | tonumber)) | select(.key | test($f)) | .id' 2>>$err_file)"
     echo $ids
 }
